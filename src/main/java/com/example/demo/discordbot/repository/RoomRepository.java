17ae3e08eb381ba51a97badf12a3808aed23d12d
DanMUZI1001 <s25001@gsm.hr.kr>
2025-12-19 09:06:55 +0900

    Initial commit

--- src/main/java/com/example/demo/discordbot/repository/RoomRepository.java
+++ src/main/java/com/example/demo/discordbot/repository/RoomRepository.java
@@ -1,6 +1,6 @@
-package com.example.demo.repository;
+package com.example.demo.discordbot.repository;
 
-import com.example.demo.ROOM;
+import com.example.demo.discordbot.ROOM;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
